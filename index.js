c02c631938f9a0c09a3c6f4cd45ed287a91f9faf
esbatusadadda <juliakomorowicz16@gmail.com>
2024-10-08 08:30:35 -0500

    last commit

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ class NobotAi {
 
   async setup() {
     const txtUserAgent = await fs.readFile("user_agent_blacklist.txt", "utf-8");
-    this.word_user_agent_blacklist = await txtUserAgent
+    this.word_user_agent_blacklist = txtUserAgent
       .trim()
       .split("\n")
       .map((word) => word.replace(/\r/g, ""));
     const txtIp = await fs.readFile("ip_blacklist.txt", "utf-8");
-    this.ip_blacklist = await txtIp
+    this.ip_blacklist = txtIp
       .trim()
       .split("\n")
       .map((word) => word.replace(/\r/g, ""));
     const txtIsp = await fs.readFile("isp_blacklist.txt", "utf-8");
-    this.isp_blacklist = await txtIsp
+    this.isp_blacklist = txtIsp
       .trim()
       .split("\n")
       .map((word) => word.replace(/\r/g, ""));
@@ -27,7 +27,7 @@ class NobotAi {
 
   async verifyUserAgent(userAgent, callback) {
     if (userAgent.length < 0) throw new Error("userAgent Required");
-    const reqUserAgent = userAgent.toLowerCase();
+    const reqUserAgent = userAgent?.toLowerCase();
     for (const word of this.word_user_agent_blacklist) {
       if (reqUserAgent.includes(word)) {
         return await callback(true, false);
@@ -48,30 +48,21 @@ class NobotAi {
     // check isp
     const res = await fetch(`http://ip-api.com/json/${ipAddress}`);
     const detailsIp = await res.json();
-    detailsIp.isp = detailsIp.isp.toLowerCase();
+    detailsIp.isp = detailsIp?.isp?.toLowerCase();
+    detailsIp.org = detailsIp?.org?.toLowerCase();
+    detailsIp.as = detailsIp?.as?.toLowerCase();
     const { isp_blacklist } = this;
     for (const isp of isp_blacklist) {
-      if (detailsIp.isp.includes(isp)) {
+      if (
+        detailsIp?.isp?.includes(isp) ||
+        detailsIp?.as?.includes(isp) ||
+        detailsIp?.org?.includes(isp)
+      ) {
         return await callback(true, false);
       }
     }
+    return await callback(false, true);
   }
 }
-
-// testing
-async function main() {
-  const scanning = new NobotAi();
-  await scanning.setup();
-  const userAgnetExample =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 msnbot/129.0.0.0";
-  const status = await scanning.verifyIpAddress(
-    "2345:0425:2CA1:0000:0000:0567:5673:23b5",
-    (robot, human) => {
-      return human;
-    }
-  );
-  console.log(status);
-}
-main();
 
 export default NobotAi;
